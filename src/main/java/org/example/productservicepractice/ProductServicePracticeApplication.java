7ac91204a379aab8114b293a4782cd6f253df7b5
Atish Maske <atish@edcast.com>
2024-05-02 23:01:52 +0530

    First Commit

---
+++ src/main/java/org/example/productservicepractice/ProductServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.productservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServicePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/productservicepractice/ProductServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
