4cdc0646511b6da95fea6fb51a8b7cb6f94a9ee1
Atish Maske <atish@edcast.com>
2024-05-04 19:06:52 +0530

    Test

--- src/main/java/org/example/productservicepractice/models/Category.java
+++ src/main/java/org/example/productservicepractice/models/Category.java
@@ -17,3 +17,7 @@ public class Category {
         this.id = Math.abs(new Random().nextLong());
     }
 }
+//git remote set-url origin https://atish23@github.com/atish23/productsServiceJava.git
+//https://github.com/atish23/productsServiceJava.git
+
+//git@github.com-personal:personal-atish23/productsServiceJava.git
